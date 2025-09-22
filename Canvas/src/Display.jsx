bba331645ab56d12f8c121c86f98efb1937303ed
Bruce <js.python03@gmail.com>
2025-09-22 14:54:04 -0700

    Fixed Color scheme

--- Canvas/src/Display.jsx
+++ Canvas/src/Display.jsx
@@ -1,7 +1,7 @@
 const Display = ({ element }) => {
     if (!element) {
         return (
-            <div className="border border-black flex items-center justify-center h-28 w-64">
+            <div className="border flex items-center justify-center h-28 w-64">
                 Click on an element to see its details
             </div>
         );
@@ -9,10 +9,10 @@ const Display = ({ element }) => {
 
     return (
         <div className="border border-black p-4 h-28 w-64">
-            <div className="font-bold text-lg text-red-500">{element.name}</div>
-            <div className="text-xs text-red-500">Symbol: {element.symbol}</div>
-            <div className="text-xs text-red-500">Atomic Number: {element.number}</div>
-            <div className="text-xs text-red-500">Atomic Mass: {element.mass}</div>
+            <div className="font-bold text-lg text-black">{element.name}</div>
+            <div className="text-xs text-black">Symbol: {element.symbol}</div>
+            <div className="text-xs text-black">Atomic Number: {element.number}</div>
+            <div className="text-xs text-black">Atomic Mass: {element.mass}</div>
         </div>
     );
 };
